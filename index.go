7a10cafff9adc56310d4b5c4b65b118850575217
nc-minh <nguyencongminh.official@gmail.com>
2022-09-21 16:09:22 +0700

    :tada: update

---
+++ index.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main()  {
+	r := gin.Default()
+
+	//static file
+	r.Static("/assets", "./assets")
+
+	//GET method
+	r.GET("/ping", getPing)
+
+	//POST method
+	r.POST("/post", postPing)
+
+	//GET method with parameter
+	r.GET("/users/:id", getUser)
+
+	//GET method with queryString
+	r.GET("/users", getUserWithQueryString)
+
+	//POST method with formdata
+	r.POST("/formdata", adminMiddleware, getUserWithForm)
+
+	//API group
+	api := r.Group("/api")
+	{
+		v1 := api.Group("/v1")
+		{
+			v1.GET("/heath", func (ctx *gin.Context)  {
+				ctx.String(http.StatusOK, "Service up!")
+			})
+		}
+	}
+
+	//upload single file
+	r.POST("/upload", func (ctx *gin.Context)  {
+		file, _ := ctx.FormFile("file")
+		ctx.SaveUploadedFile(file, "./assets/upload/" + file.Filename)
+		ctx.String(http.StatusOK, file.Filename + " uploaded!")
+	})
+
+	//upload multiple files
+	r.POST("/uploads", func (ctx *gin.Context)  {
+		form, _ := ctx.MultipartForm()
+		files := form.File["files"]
+		
+		for _, file := range files {
+			ctx.SaveUploadedFile(file, "./assets/upload/" + file.Filename)
+		}
+
+		ctx.String(http.StatusOK, "files uploaded!")
+	})
+
+	//Run server
+	r.Run(":3333")
+}
+
+func getPing(ctx *gin.Context) {
+	ctx.String(http.StatusOK, "pong")
+}
+
+func postPing(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+			"message": "This is a post request",
+		})
+}
+
+func getUser(ctx *gin.Context) {
+	id := ctx.Param("id")
+	ctx.String(http.StatusOK, "User ID: " + id)
+}
+
+func getUserWithQueryString(ctx *gin.Context) {
+	name := ctx.Query("name")
+	age := ctx.Query("age")
+	ctx.String(http.StatusOK, "Name: " + name + " Age: " + age)
+}
+
+func getUserWithForm(ctx *gin.Context) {
+	name := ctx.PostForm("name")
+	age := ctx.PostForm("age")
+	ctx.String(http.StatusOK, "Name: " + name + " Age: " + age)
+}
+
+//Middleware
+func adminMiddleware(ctx *gin.Context) {
+	name := ctx.PostForm("name")
+
+	if(name != "admin") {
+		ctx.JSON(http.StatusUnauthorized, gin.H{
+			"message": "You are not an admin",
+		})
+		ctx.Abort()
+		return
+	}
+
+	ctx.Next()
+	
+}
